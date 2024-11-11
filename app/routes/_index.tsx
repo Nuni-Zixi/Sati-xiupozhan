bceaf65cd9c0d5899ef9020a13756e5d15ccc359
Nuni. <119395350+Nuni-Zixi@users.noreply.github.com>
2024-11-11 16:54:07 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -39,7 +39,7 @@ export default function Index() {
             <li>
               <Link
                 to="/generate-image"
-                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-white to-pink-300 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-pink to-white-300 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 AI Image Creat
               </Link>
