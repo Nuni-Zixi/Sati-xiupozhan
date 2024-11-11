5d2cc091358aef97a9bb6cd134d36b2006f4e739
Nuni. <119395350+Nuni-Zixi@users.noreply.github.com>
2024-11-11 23:31:18 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -44,11 +44,12 @@ export default function Index() {
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-pink-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 AI绘图 少女绘画中~
-              </Link>
+             <Link
               to="https://bpb.jizixi.com/panel"
-              className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-purple-300 to-pink rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-purple-300 to-pink rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 少女の魔法~
+              </Link>
             </li>
             {/* 可以在这里添加更多的导航项 */}
           </ul>
