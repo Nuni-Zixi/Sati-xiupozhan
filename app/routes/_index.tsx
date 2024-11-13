1594bc2d951317d448c0ab8eb923944223f0633f
Nuni. <119395350+Nuni-Zixi@users.noreply.github.com>
2024-11-13 16:08:38 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,18 +46,18 @@ export default function Index() {
               </li>
             <li>
               <Link
-                to="https://bpb.jizixi.com/panel"
-                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-purple-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
-              >
-                少女の魔法~
-              </Link>
-              </li>
-            <li>
-              <Link
                 to="https://proxy.jizixi.com/https://duckduckgo.com/chat"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-violet-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 达达鸭AI 少女の推荐~
+             </Link>
+              </li>
+            <li>
+              <Link
+                to="https://bpb.jizixi.com/panel"
+                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-purple-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
+              >
+                少女の魔法~
               </Link>
               </li>
             <li>
