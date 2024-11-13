706ae4b8d50398537d9b4cf8dfdcbb6f449af20f
Nuni. <119395350+Nuni-Zixi@users.noreply.github.com>
2024-11-13 16:10:53 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -47,7 +47,7 @@ export default function Index() {
             <li>
               <Link
                 to="https://proxy.jizixi.com/https://duckduckgo.com/chat"
-                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-Crimson-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-BlueViolet-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 达达鸭AI 少女の推荐~
                 </Link>
