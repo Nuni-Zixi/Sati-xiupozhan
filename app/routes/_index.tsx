b00c6644ef822e4b60aada4ed20bd1b21d3c14f1
Nuni. <119395350+Nuni-Zixi@users.noreply.github.com>
2024-11-16 01:11:17 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -82,6 +82,7 @@ export default function Index() {
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-black bg-gradient-to-r from-blue-300 to-white rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-400"
               >
                 Satiの磁力搜索~
+                </Link>
                 </li>
             <li>
               <Link
